9532b89adf1a932c69698ac5b19dfd700769eac4
MasumAM1803 <masum.abdulm@gmail.com>
2024-05-30 23:53:14 +0700

    Add test routing

--- app.vue
+++ app.vue
@@ -6,13 +6,23 @@ onMounted(() => {
     initFlowbite();
 })
 
-const { data: test } = await useFetch('https://dummyjson.com/test');
+// const { data: test } = await useFetch('https://dummyjson.com/test');
 </script>
 
 <template>
-  <div class="absolute flex justify-center items-center h-full w-full">
-    <!-- <ModalDefault /> -->
-    <!-- <p>Page visits: {{ test.status }}</p> -->
-    <LoginCard />
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/">Login</NuxtLink></li>
+          <li><NuxtLink to="/product">Product</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <div class="absolute flex justify-center items-center h-full w-full">
+      <!-- <ModalDefault /> -->
+      <!-- <p>Page visits: {{ test.status }}</p> -->
+      <NuxtPage />
+    </div>
   </div>
 </template>
