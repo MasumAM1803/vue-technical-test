7f7fc5aac4ca737fbd7eef99f34f29a88c49d1b6
MasumAM1803 <masum.abdulm@gmail.com>
2024-05-30 22:58:49 +0700

    testing api

--- app.vue
+++ app.vue
@@ -1,14 +1,17 @@
-<script setup>
+<script setup lang="ts">
 import { onMounted } from 'vue'
 import { initFlowbite } from 'flowbite'
 
 onMounted(() => {
     initFlowbite();
 })
+
+const { data: test } = await useFetch('https://dummyjson.com/test');
 </script>
 
 <template>
   <div>
     <ModalDefault />
+    <p>Page visits: {{ test.status }}</p>
   </div>
 </template>
